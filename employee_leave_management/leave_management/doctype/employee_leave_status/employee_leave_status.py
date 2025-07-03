2d77b40577d5d7dcce2c926131c793bc494bc154
akashlogics <akashaiprompt650@email.com>
2025-07-03 17:14:26 +0530

    added month

--- employee_leave_management/leave_management/doctype/employee_leave_status/employee_leave_status.py
+++ employee_leave_management/leave_management/doctype/employee_leave_status/employee_leave_status.py
@@ -5,14 +5,30 @@ import frappe
 from frappe.utils import flt
 from frappe.model.document import Document
 
-
 class EmployeeLeaveStatus(Document):
 	def validate(self):
-		self.update_leave_usage()
+		self.calculate_leave_usage()
+		self.calculate_remaining_leaves()
+		self.validate_allocation()
 
-	def update_leave_usage(self):
-		count=frappe.db.sum('Leave Applications', {
-			'employee': self.employee,
-			'status_update': 'Approved'
-		})
-		self.leaves_used = count
+	def calculate_leave_usage(self):
+		leaves_used = frappe.db.sql("""
+			SELECT COALESCE(SUM(total_leave_days), 0)
+			FROM `tabLeave Applications`
+			WHERE employee = %s
+			  AND month = %s
+			  AND status = 'Approved'
+			  AND docstatus = 1
+		""", (self.employee, self.month))
+
+		self.leaves_used = flt(leaves_used[0][0]) if leaves_used else 0
+
+	def calculate_remaining_leaves(self):
+		"""Calculate remaining leaves from allocation and used"""
+		self.leaves_remaining = flt(self.leave_allocated) - flt(self.leaves_used)
+
+	def validate_allocation(self):
+		if not self.leave_allocated:
+			frappe.throw("Leave Allocation is required")
+		if flt(self.leave_allocated) < 0:
+			frappe.throw("Leave Allocation cannot be negative")
---
+++ employee_leave_management/leave_management/notification/leave_applications_submission/leave_applications_submission.md
@@ -0,0 +1,8 @@
+<p>Dear Manager,</p>
+
+<p>A new Leave Application has been submitted by {{ doc.employee }}.</p>
+
+<p>Please review and take appropriate action.</p>
+
+<p>Regards,
+HR System</p>
